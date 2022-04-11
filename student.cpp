538e011dc2809810073dd4de0e002e674fed1db0
kevman967 <kevman966@gmail.com>
2022-04-11 19:00:32 -0400

    master

---
+++ student.cpp
@@ -0,0 +1,6 @@
+#include <"student.h"> 
+// include getter and setter for every attribute
+student::student()
+{
+
+}
---
+++ student.h
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+class student{
+public:
+student();
+
+private:
+string firstname;
+string lastname;
+int id;
+string birth;
+double gpa;
+int startyear;
+int completed_credit;
+string program;
+};
